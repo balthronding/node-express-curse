0af6ae8a58c749bb9d7a6e0183e769d193d2e784
Juan Gabriel González Gálvez <juanggg1983@gmail.com>
2021-01-04 18:28:11 +0100

    Cambios en la lista de productos

--- frontend/src/app/components/lista-productos/lista-productos.component.ts
+++ frontend/src/app/components/lista-productos/lista-productos.component.ts
@@ -11,22 +11,33 @@ export class ListaProductosComponent implements OnInit {
 
   lista: IProducto[];
 
-  constructor(private router:Router) {
+  constructor(private router: Router) {
     this.lista = [
-      {titulo:'producto 1', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:5},
-      {titulo:'producto 2', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:1},
-      {titulo:'producto 3', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:10},
-      {titulo:'producto 4', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:10},
-      {titulo:'producto 5', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:1},
-      {titulo:'producto 6', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:5},
-      {titulo:'producto 7', descripcion:"XXX", idUsuario:"moipin", enlace:'https://www.amazon.es',puntuacion:5},
+      { identificador: '1', titulo: 'producto 1', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 5 },
+      { identificador: '1', titulo: 'producto 2', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 1 },
+      { identificador: '1', titulo: 'producto 3', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 10 },
+      { identificador: '1', titulo: 'producto 4', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 10 },
+      { identificador: '1', titulo: 'producto 5', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 1 },
+      { identificador: '1', titulo: 'producto 6', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 5 },
+      { identificador: '1', titulo: 'producto 7', descripcion: "XXX", idUsuario: "moipin", enlace: 'https://www.amazon.es', puntuacion: 5 },
     ]
   }
 
   ngOnInit(): void {
   }
 
-  addProducto(): void{
+  addProducto(): void {
     this.router.navigate(['anadir-producto'])
   }
+
+  editarProducto(identificador: string): void {
+
+  }
+
+  deleteProducto(identificador: string): void {
+    if (confirm('¿Estás seguro de que quieres eliminar esta entrada?')) {
+
+    }
+  }
+
 }
